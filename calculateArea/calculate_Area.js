5d429ee60874c11866631c983f4b8eeba17976ea
dennismuhnene <dennis.cmuhnene@gmail.com>
2025-05-10 06:35:51 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea() {
 let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1
+let grocery2
+let grocer3
+
+function calculategroceries() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+let amount = grocery1 + grocery2 + grocery3
+document.getElementById('result').innerText = `The area total amount is: ${amount}`;
+}
