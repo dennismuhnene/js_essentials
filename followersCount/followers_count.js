e3cf8c6184d6b965c86338f09f907fd440f3d0bc
dennismuhnene <dennis.cmuhnene@gmail.com>
2025-05-10 09:29:27 -0400

    message

---
+++ followersCount/followers_count.js
@@ -0,0 +1,29 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+    count++; // Increment the count by 1
+}
+
+function displayCount() {
+document.getElementById('countDisplay').innerHTML = count; // Displays the count in the HTML
+}
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your insatgram post gained 10 follower! Congratulations!");
+    }
+    else if (count === 20) {
+        alert("Your instagram account gained 20 followers! Keep it up!")
+    }
+}
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); //Displays the count
+    checkCountValue(); // Check count value and display message
+}
