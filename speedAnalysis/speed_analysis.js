27bbcdee612c82ddee6b98b164d4065c66c7ceb9
dennismuhnene <dennis.cmuhnene@gmail.com>
2025-05-13 13:05:00 -0400

    message

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -32,6 +32,8 @@ button.onclick = endTest;
 
             var wpm = 0; // Default value
 
+            var wordlen = userTypedText.length;
+
             if (timeElapsed !== 0 && !isNaN(typedWords)) {
                 wpm = Math.round((typedWords / timeElapsed) * 60);
             }
@@ -39,7 +41,8 @@ button.onclick = endTest;
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
-                "<p>Words Typed: " + typedWords + "</p>" +
+            "<p>Words Length: " + wordlen + "</p>" +    
+            "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
 
